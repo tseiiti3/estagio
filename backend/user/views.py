38893cafa27ddb0548e9832e6e9d9833d24b79fe
seiiti <tseiiti@gmail.com>
2026-03-22 19:51:48 -0300

    configuração básica backend e apis

---
+++ backend/user/views.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import User
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .serializers import UserSerializer
+
+class CreateUserView(generics.CreateAPIView):
+  queryset = User.objects.all()
+  serializer_class = UserSerializer
+  permission_classes = [ AllowAny ]
